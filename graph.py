7ea74ab0840ce1da8a831baf65514d11632be422
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2022-11-11 15:52:32 +0200

    Summary
    Finishing some pylint details

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
+"""get graph coords from a file"""
 def get_graph_from_file(file_name):
     """
     (str) -> (list)
 
     Read graph from file and return a list of edges.
 
-    >>> get_graph_from_file('D:\labs\lab7\Pelekh-Anastasiia-lab7-task1\data1.txt')
+    >>> get_graph_from_file('data1.txt')
     [[1, 2], [3, 4], [1, 5]]
     """
     with open(file_name, 'r', encoding='utf-8') as file:

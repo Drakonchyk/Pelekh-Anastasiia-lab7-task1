b54559fc9a48fad25677b7ad1e5841320a5c233f
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2022-11-11 16:19:48 +0200

    Create add_node.py
    basis

---
+++ add_node.py
@@ -0,0 +1,15 @@
+"""adiing a node"""
+def add_node(graph, node):
+    """
+    (dict, int) -> (dict)
+
+    Add a new node to the graph and return a new graph.
+
+    >>> add_node({1: [2], 2: [1]}, 3)
+    {1: [2], 2: [1], 3: []}
+    """
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())

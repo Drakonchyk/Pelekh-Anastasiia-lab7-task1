11eb10f129f11612e4a5feb54cdbb96e02d6d818
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2022-11-12 21:04:46 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,14 +1,30 @@
 """writing edges into a dictionary"""
+
 def to_edge_dict(edge_list):
     """
     (list) -> (dict)
-
     Convert a graph from list of edges to dictionary of vertices.
-
     >>> to_edge_dict([[1, 2], [3, 4], [1, 5], [2, 4]])
-    {1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}
+    {1: [2, 5], 2: [1, 4], 3: [4], 4: [3, 2], 5: [1]}
     """
-    pass
+    dictionary = {}
+
+    for edge in edge_list:
+        first_edge = edge[0]
+        second_edge = edge[1]
+
+        if first_edge in dictionary:
+            dictionary[first_edge].append(second_edge)
+        else:
+            dictionary.setdefault(first_edge, [second_edge])
+
+        if second_edge in dictionary:
+            dictionary[second_edge].append(first_edge)
+        else:
+            dictionary.setdefault(second_edge, [first_edge])
+
+    return dictionary
+
 
 if __name__ == '__main__':
     import doctest

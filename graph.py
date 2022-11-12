20c12737d6481d18051ebd05756ce06906481bdc
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2022-11-12 22:35:14 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,53 @@
-"""adding edges"""
+"""get graph coords from a file"""
+def get_graph_from_file(file_name):
+    """
+    (str) -> (list)
+
+    Read graph from file and return a list of edges.
+
+    >>> get_graph_from_file('data1.txt')
+    [[1, 2], [3, 4], [1, 5]]
+    """
+    with open(file_name, 'r', encoding='utf-8') as file:
+        list_coords = []
+        for line in file:
+            list_coords.append([int(line[0]), int(line[2])])
+    return list_coords
+
+
+def to_edge_dict(edge_list):
+    """
+    (list) -> (dict)
+    Convert a graph from list of edges to dictionary of vertices.
+    >>> to_edge_dict([[1, 2], [3, 4], [1, 5], [2, 4]])
+    {1: [2, 5], 2: [1, 4], 3: [4], 4: [3, 2], 5: [1]}
+    """
+    dictionary = {}
+
+    for edge in edge_list:
+        first_edge = edge[0]
+        second_edge = edge[1]
+        if first_edge in dictionary:
+            dictionary[first_edge].append(second_edge)
+        else:
+            dictionary.setdefault(first_edge, [second_edge])
+
+        if second_edge in dictionary:
+            dictionary[second_edge].append(first_edge)
+        else:
+            dictionary.setdefault(second_edge, [first_edge])
+    return dictionary
+
+
+def is_edge_in_graph(graph, edge):
+    """
+    (dict, tuple) -> bool
+    Return True if graph contains a given edge and False otherwise.
+    >>> is_edge_in_graph({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (3, 1))
+    False
+    """
+    return edge[1] in graph[edge[0]]
+
 
 def add_edge(graph, edge):
     """
@@ -23,6 +72,60 @@ def add_edge(graph, edge):
         graph.setdefault(second_edge, [first_edge])
 
     return graph
+
+def del_edge(graph, edge):
+    """
+    (dict, tuple) -> (dict)
+    Delete an edge from the graph and return a new graph.
+    >>> del_edge({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, (2, 4))
+    {1: [2, 5], 2: [1], 3: [4], 4: [3], 5: [1]}
+    """
+    first_edge = edge[0]
+    second_edge = edge[1]
+
+    if first_edge in graph:
+        graph[first_edge].remove(second_edge)
+
+    if second_edge in graph:
+        graph[second_edge].remove(first_edge)
+
+    return graph
+
+def add_node(graph, node):
+    """
+    (dict, int) -> (dict)
+    Add a new node to the graph and return a new graph.
+    >>> add_node({1: [2], 2: [1]}, 3)
+    {1: [2], 2: [1], 3: []}
+    """
+    graph.setdefault(node, [])
+    return graph
+
+def del_node(graph, node):
+    """
+    (dict, int) -> (dict)
+    Delete a node and all incident edges from the graph.
+    >>> del_node({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}, 4)
+    {1: [2, 5], 2: [1], 3: [], 5: [1]}
+    """
+    del graph[node]
+    for values in graph:
+        if node in graph[values]:
+            graph[values].remove(node)
+    return graph
+
+def convert_to_dot(graph):
+    """
+    (dict) -> (None)
+    Save the graph to a file in a DOT format.
+    >>> convert_to_dot({1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]})
+    """
+    with open('graph.dot', 'w', encoding='utf-8') as file:
+        file.write("graph{\n")
+        for key in graph:
+            for value in graph[key]:
+                file.write(str(key) + "--" + str(value) + "\n")
+        file.write("}")
 
 
 if __name__ == '__main__':

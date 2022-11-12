ae4b9531c26c2fe8d17ccc7dc4e160bded7fd176
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2022-11-12 21:16:50 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,14 +1,14 @@
-"""adiing a node"""
+"""adding a node"""
+
 def add_node(graph, node):
     """
     (dict, int) -> (dict)
-
     Add a new node to the graph and return a new graph.
-
     >>> add_node({1: [2], 2: [1]}, 3)
     {1: [2], 2: [1], 3: []}
     """
-    pass
+    graph.setdefault(node, [])
+    return graph
 
 if __name__ == '__main__':
     import doctest
